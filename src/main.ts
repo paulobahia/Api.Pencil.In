2d223f7208b5bb7698726f7d6df688878e80f69f
Paulo Bahia <pauloipatinga22@gmail.com>
2024-09-07 12:41:09 -0300

    Feat: Implementação e configuração do Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ credentials: true })
+
+  const config = new DocumentBuilder()
+    .setTitle('Pencil.In API')
+    .setDescription('API para gerenciar os agendamentos da plataforma Pencil.In')
+    .setVersion('1.0')
+    .addTag('endpoints')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
   await app.listen(5001);
 }
 bootstrap();
