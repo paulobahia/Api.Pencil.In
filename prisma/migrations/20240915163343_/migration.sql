1bcf9c795ef1e2b11cf4d78cfde1961f6dca001d
Paulo Bahia <pauloipatinga22@gmail.com>
2024-09-15 13:35:15 -0300

    Chore: Alteração no Schema

--- prisma/migrations/20240915163343_/migration.sql
+++ prisma/migrations/20240915163343_/migration.sql
@@ -8,7 +8,7 @@ CREATE TYPE "CalendarType" AS ENUM ('GOOGLE_CALENDAR', 'OUTLOOK_CALENDAR', 'APPL
 CREATE TYPE "SchedulingStatus" AS ENUM ('PENDING', 'CONFIRMED', 'CANCELLED');
 
 -- CreateTable
-CREATE TABLE "Establishment" (
+CREATE TABLE "Studio" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "address" TEXT,
@@ -19,7 +19,7 @@ CREATE TABLE "Establishment" (
     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
     "deletedAt" TIMESTAMP(3),
 
-    CONSTRAINT "Establishment_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Studio_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -28,7 +28,7 @@ CREATE TABLE "Employee" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "establishmentId" TEXT NOT NULL,
+    "studioId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
@@ -44,6 +44,7 @@ CREATE TABLE "OperatingHour" (
     "dayOfWeek" "DayOfWeek" NOT NULL,
     "specificDate" TIMESTAMP(3),
     "isAbsence" BOOLEAN NOT NULL DEFAULT false,
+    "isDefault" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
@@ -53,9 +54,21 @@ CREATE TABLE "OperatingHour" (
 );
 
 -- CreateTable
+CREATE TABLE "OperatingHourException" (
+    "id" TEXT NOT NULL,
+    "operatingHourId" TEXT NOT NULL,
+    "exceptionDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "OperatingHourException_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "TimeInterval" (
     "id" TEXT NOT NULL,
     "operatingHourId" TEXT NOT NULL,
+    "operatingHourExceptionId" TEXT,
     "startTime" TIMESTAMP(3) NOT NULL,
     "endTime" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -69,7 +82,7 @@ CREATE TABLE "TimeInterval" (
 -- CreateTable
 CREATE TABLE "Service" (
     "id" TEXT NOT NULL,
-    "establishmentId" TEXT NOT NULL,
+    "studioId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT,
     "durationMinutes" INTEGER NOT NULL,
@@ -80,6 +93,19 @@ CREATE TABLE "Service" (
     "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "Service_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "EmployeeService" (
+    "id" TEXT NOT NULL,
+    "employeeId" TEXT NOT NULL,
+    "serviceId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "EmployeeService_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -114,7 +140,7 @@ CREATE TABLE "Scheduling" (
 -- CreateTable
 CREATE TABLE "CalendarIntegration" (
     "id" TEXT NOT NULL,
-    "establishmentId" TEXT NOT NULL,
+    "studioId" TEXT NOT NULL,
     "calendarType" "CalendarType" NOT NULL,
     "eventId" TEXT NOT NULL,
     "status" TEXT,
@@ -147,16 +173,28 @@ CREATE UNIQUE INDEX "Employee_email_key" ON "Employee"("email");
 CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
 
 -- AddForeignKey
-ALTER TABLE "Employee" ADD CONSTRAINT "Employee_establishmentId_fkey" FOREIGN KEY ("establishmentId") REFERENCES "Establishment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_studioId_fkey" FOREIGN KEY ("studioId") REFERENCES "Studio"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "OperatingHour" ADD CONSTRAINT "OperatingHour_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "OperatingHourException" ADD CONSTRAINT "OperatingHourException_operatingHourId_fkey" FOREIGN KEY ("operatingHourId") REFERENCES "OperatingHour"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "TimeInterval" ADD CONSTRAINT "TimeInterval_operatingHourId_fkey" FOREIGN KEY ("operatingHourId") REFERENCES "OperatingHour"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Service" ADD CONSTRAINT "Service_establishmentId_fkey" FOREIGN KEY ("establishmentId") REFERENCES "Establishment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "TimeInterval" ADD CONSTRAINT "TimeInterval_operatingHourExceptionId_fkey" FOREIGN KEY ("operatingHourExceptionId") REFERENCES "OperatingHourException"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Service" ADD CONSTRAINT "Service_studioId_fkey" FOREIGN KEY ("studioId") REFERENCES "Studio"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EmployeeService" ADD CONSTRAINT "EmployeeService_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EmployeeService" ADD CONSTRAINT "EmployeeService_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Scheduling" ADD CONSTRAINT "Scheduling_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -165,7 +203,7 @@ ALTER TABLE "Scheduling" ADD CONSTRAINT "Scheduling_userId_fkey" FOREIGN KEY ("u
 ALTER TABLE "Scheduling" ADD CONSTRAINT "Scheduling_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CalendarIntegration" ADD CONSTRAINT "CalendarIntegration_establishmentId_fkey" FOREIGN KEY ("establishmentId") REFERENCES "Establishment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "CalendarIntegration" ADD CONSTRAINT "CalendarIntegration_studioId_fkey" FOREIGN KEY ("studioId") REFERENCES "Studio"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Absence" ADD CONSTRAINT "Absence_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE CASCADE ON UPDATE CASCADE;
